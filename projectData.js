6af65c2f4bdc0125c3d492a38544b2f7d20e62bd
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-02-08 14:08:45 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -1,27 +1,17 @@
 
 const projectsData = [
     {
-        imgSrc: "/material/ProofAI.png",
+        imgSrc: "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/ProofAI.png",
         imgAlt: "ProofAI Project",
         githubLink: "https://github.com/yourusername/proofai",
         hostUrl: "https://yourwebsite.com",
         title: "ProofAI - Train AI Models Using Proof of Work",
         description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: ["/material/ipfs.svg", "/material/Go.svg", "/material/react-icon.svg"],
+        techIcon: ["https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/ipfs.svg", "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/Go.svg", "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/react-icon.svg"],
         techIconAlt: ["IPFS", "Go", "React"],
         techName: ["IPFS", "Go", "React", "Decentralized"]
     },
-    {
-        imgSrc: "/material/ProofAI.png",
-        imgAlt: "ProofAI Project",
-        githubLink: "https://github.com/yourusername/proofai",
-        hostUrl: "https://yourwebsite.com",
-        title: "ProofAI - Train AI Models Using Proof of Work",
-        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: ["/material/ipfs.svg", "/material/Go.svg", "/material/react-icon.svg"],
-        techIconAlt: ["IPFS", "Go", "React"],
-        techName: ["IPFS", "Go", "React", "Decentralized"]
-    }
+   
 ];
 
 const ProjectCategory = [
