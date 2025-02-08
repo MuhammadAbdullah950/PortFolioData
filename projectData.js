899bf9b3c984fd8d9e99907a89927117b818c2c3
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-02-08 14:17:46 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -1,13 +1,13 @@
 
 const projectsData = [
     {
-        imgSrc: "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/ProofAI.png",
+        imgSrc: "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ProofAI.png",
         imgAlt: "ProofAI Project",
         githubLink: "https://github.com/yourusername/proofai",
         hostUrl: "https://yourwebsite.com",
         title: "ProofAI - Train AI Models Using Proof of Work",
         description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: ["https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/ipfs.svg", "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/Go.svg", "https://github.com/MuhammadAbdullah950/PortFolioData/blob/main/ProofAI/react-icon.svg"],
+        techIcon: ["https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"],
         techIconAlt: ["IPFS", "Go", "React"],
         techName: ["IPFS", "Go", "React", "Decentralized"]
     },
