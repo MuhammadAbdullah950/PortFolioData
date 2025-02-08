43bdeac6552550817daf0017318bec8fd33dc621
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-02-08 13:37:41 +0500

    Create projectData.js

---
+++ projectData.js
@@ -0,0 +1,33 @@
+
+const projectsData = [
+    {
+        imgSrc: "/material/ProofAI.png",
+        imgAlt: "ProofAI Project",
+        githubLink: "https://github.com/yourusername/proofai",
+        hostUrl: "https://yourwebsite.com",
+        title: "ProofAI - Train AI Models Using Proof of Work",
+        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
+        techIcon: ["/material/ipfs.svg", "/material/Go.svg", "/material/react-icon.svg"],
+        techIconAlt: ["IPFS", "Go", "React"],
+        techName: ["IPFS", "Go", "React", "Decentralized"]
+    },
+    {
+        imgSrc: "/material/ProofAI.png",
+        imgAlt: "ProofAI Project",
+        githubLink: "https://github.com/yourusername/proofai",
+        hostUrl: "https://yourwebsite.com",
+        title: "ProofAI - Train AI Models Using Proof of Work",
+        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
+        techIcon: ["/material/ipfs.svg", "/material/Go.svg", "/material/react-icon.svg"],
+        techIconAlt: ["IPFS", "Go", "React"],
+        techName: ["IPFS", "Go", "React", "Decentralized"]
+    }
+];
+
+const ProjectCategory = [
+    { value: "all", label: "All Projects" },
+    { value: "web", label: "Web Development" },
+    { value: "mobile", label: "Mobile Apps" },
+    { value: "desktop", label: "Desktop Apps" },
+]
+export  { projectsData ,  ProjectCategory};
