498a219bda05494346afd576335e6c9f05f6cdfc
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-02-08 22:48:20 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -12,18 +12,7 @@ const projectsData = [
         techName: ["IPFS", "Go", "React", "Decentralized"],
         category : "desktop",
     },
-    {
-        imgSrc: "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ProofAI.png",
-        imgAlt: "ProofAI Project",
-        githubLink: "https://github.com/yourusername/proofai",
-        hostUrl: "https://yourwebsite.com",
-        title: "ProofAI - Train AI Models Using Proof of Work",
-        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: ["https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"],
-        techIconAlt: ["IPFS", "Go", "React"],
-        techName: ["IPFS", "Go", "React", "Decentralized"],
-        category : "desktop",
-    },
+    
    
 ];
 
