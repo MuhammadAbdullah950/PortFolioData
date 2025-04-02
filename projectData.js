285280d4b504e6425d90b96638263b71372ec8d2
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-04-02 22:33:47 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -17,7 +17,7 @@ const projectsData = [
     },
     {
         title: "ArbiFlash Bot - DeFi Arbitrage Tool for Cross-Exchange Flash Loan Trading",
-        description: "ArbiFlash Bot automates cryptocurrency arbitrage trading using flash loans. Monitor price differences across multiple DEXs, execute profitable trades automatically, and analyze performance with real-time metrics - all in one intuitive dashboard.",
+        description: "Automated crypto arbitrage bot using flash loans to track DEX prices and execute profitable trades in real time.",
         videoSrc: "https://drive.google.com/file/d/1ogtRepUyOsAZle2866aeKd0yCc-C_eGq/preview?pli=1",
         techName: ["Smart Contracts", "React", "Decentralized Finance"],
         category: "dApps",
@@ -28,7 +28,15 @@ const projectsData = [
         videoSrc: "https://drive.google.com/file/d/1KxKpQvCNre8-EiCPp0qZ1ARf7IgM3RjT/preview?pli=1",
         techName: ["Ethereum Smart Contract", "React", "Decentralized Governance"],
         category: "dApps",
+    },
+     {
+        title: "Stakify Crypto Wallet: Hold, Purchase, Trade, and Earn Through Staking",
+        description: "A secure crypto wallet for storing, trading, and staking digital assets with decentralized governance.",
+        videoSrc: "https://drive.google.com/file/d/1MP-380kJqNHSxNFmnZ88y8huMn31f0cm/preview",
+        techName: ["Blockchain", "React", "Decentralized Finance"],
+        category: "dApps",
     }
+
 ];
 
 const ProjectCategory = [
