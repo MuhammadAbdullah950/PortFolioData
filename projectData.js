141aed3fb5af2e59b361450985343c06cbe9f201
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-04-02 20:43:48 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -2,21 +2,24 @@ const projectsData = [
     {
         imgSrc: "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/proofai_up.png",
         imgAlt: "ProofAI Project",
-        videoSrc: null ,
-        isVideo : false,
+        videoSrc: null,  // Set to null if there's no video
+        isVideo: false,   // Use ':' instead of '='
         githubLink: "https://github.com/MuhammadAbdullah950/ProofAI",
         hostUrl: "https://github.com/MuhammadAbdullah950/ProofAI/releases/tag/v1.0.0-beta",
         title: "ProofAI - Revolutionizing AI Training with Proof of Work and Zero-Layer Blockchain Integration",
         description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: ["https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"],
+        techIcon: [
+            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg",
+            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg",
+            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"
+        ],
         techIconAlt: ["IPFS", "Go", "React"],
         techName: ["IPFS", "Go", "React", "Decentralized"],
-        category : "desktop",
+        category: "desktop",
     },
-    
-   
 ];
+
 const ProjectCategory = [
     { value: "all", label: "All Projects" },
     { value: "desktop", label: "dApps" },
-]
+];
