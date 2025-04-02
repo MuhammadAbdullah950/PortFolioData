6e55c3b2573caf7964b2a0ec6be0e4335c2f9e2f
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-04-02 23:11:28 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -18,11 +18,24 @@ const projectsData = [
     {
         title: "ArbiFlash Bot - DeFi Arbitrage Tool for Cross-Exchange Flash Loan Trading",
         description: "Automated crypto arbitrage bot using flash loans to track DEX prices and execute profitable trades in real time.",
-        videoSrc: "./Arbitrage_FlashLoan_bot_updated2.mp4",
+        videoSrc: "https://drive.google.com/file/d/1ogtRepUyOsAZle2866aeKd0yCc-C_eGq/preview?pli=1",
         techName: ["Smart Contracts", "React", "Decentralized Finance"],
         category: "dApps",
     },
-   
+    {
+        title: "Vegan Rob's DAO: Decentralized Governance Platform",
+        description: "Vegan Rob's DAO enables token holders to govern via a blockchain platform, offering election creation, voting, and membership tracking with transparency.",     
+        videoSrc: "https://drive.google.com/file/d/1KxKpQvCNre8-EiCPp0qZ1ARf7IgM3RjT/preview?pli=1",
+        techName: ["Ethereum Smart Contract", "React", "Decentralized Governance"],
+        category: "dApps",
+    },
+     {
+        title: "Stakify Crypto Wallet: Hold, Purchase, Trade, and Earn Through Staking",
+        description: "A secure crypto wallet for storing, trading, and staking digital assets with decentralized governance.",
+        videoSrc: "https://drive.google.com/file/d/1MP-380kJqNHSxNFmnZ88y8huMn31f0cm/preview?pli=1",
+        techName: ["Blockchain", "React", "Decentralized Finance"],
+        category: "dApps",
+    }
 
 ];
 
