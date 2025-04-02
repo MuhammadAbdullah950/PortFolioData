aea7053814d0813c37e29a7203472199bccf676b
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-04-02 20:59:23 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -1,13 +1,10 @@
 const projectsData = [
     {
+        title: "ProofAI - Blockchain Integrated AI Training",
+        description: "An innovative platform integrating blockchain’s proof-of-work mechanism with AI model training.",
         imgSrc: "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/proofai_up.png",
-        imgAlt: "ProofAI Project",
-        videoSrc: null,  // Set to null if there's no video
-        isVideo: false,   // Use ':' instead of '='
         githubLink: "https://github.com/MuhammadAbdullah950/ProofAI",
         hostUrl: "https://github.com/MuhammadAbdullah950/ProofAI/releases/tag/v1.0.0-beta",
-        title: "ProofAI - Revolutionizing AI Training with Proof of Work and Zero-Layer Blockchain Integration",
-        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
         techIcon: [
             "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg",
             "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg",
@@ -17,9 +14,20 @@ const projectsData = [
         techName: ["IPFS", "Go", "React", "Decentralized"],
         category: "desktop",
     },
+    {
+        title: "AI Video Processor",
+        description: "A tool for AI-powered video editing and enhancements.",
+        videoSrc: "https://drive.google.com/uc?export=download&id=1ogtRepUyOsAZle2866aeKd0yCc-C_eGq",  // Replace YOUR_VIDEO_ID
+        
+        techName: ["AI", "OpenCV", "Machine Learning"],
+        category: "video",
+    }
 ];
 
-const ProjectCategory = [
+const projectCategories = [
     { value: "all", label: "All Projects" },
     { value: "desktop", label: "dApps" },
+    { value: "video", label: "AI Video Processing" }
 ];
+
+export { projectsData, projectCategories };
