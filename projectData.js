1e8739c0289f82e39a67209269b7d803f6097ca0
Muhammad Abdullah <abdullah.bhatti.990011@gmail.com>
2025-04-02 20:39:07 +0500

    Update projectData.js

--- projectData.js
+++ projectData.js
@@ -2,43 +2,21 @@ const projectsData = [
     {
         imgSrc: "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/proofai_up.png",
         imgAlt: "ProofAI Project",
-        videoSrc: "",
-        isVideo: false,
-        isVimeo: false,
+        videoSrc: ,
+        isVideo = false,
         githubLink: "https://github.com/MuhammadAbdullah950/ProofAI",
         hostUrl: "https://github.com/MuhammadAbdullah950/ProofAI/releases/tag/v1.0.0-beta",
         title: "ProofAI - Revolutionizing AI Training with Proof of Work and Zero-Layer Blockchain Integration",
         description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: [
-            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", 
-            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", 
-            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"
-        ],
+        techIcon: ["https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/react-icon.svg"],
         techIconAlt: ["IPFS", "Go", "React"],
         techName: ["IPFS", "Go", "React", "Decentralized"],
-        category: "desktop",
+        category : "desktop",
     },
-    {
-        imgSrc: "",
-        imgAlt: "ProofAI Video Demo",
-        videoSrc: "https://vimeo.com/1071829584?share=copy",
-        isVideo: true,
-        isVimeo: true,
-        githubLink: "",
-        hostUrl: "",
-        title: "ProofAI - Revolutionizing AI Training with Proof of Work and Zero-Layer Blockchain Integration",
-        description: "An innovative platform integrating blockchain's proof-of-work mechanism with AI model training.",
-        techIcon: [
-            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/ipfs.svg", 
-            "https://raw.githubusercontent.com/MuhammadAbdullah950/PortFolioData/main/ProofAI/Go.svg", 
-        ],
-        techIconAlt: ["IPFS", "Go", "React"],
-        techName: ["IPFS", "Go", "React", "Decentralized"],
-        category: "desktop",
-    }
+    
+   
 ];
-
 const ProjectCategory = [
     { value: "all", label: "All Projects" },
     { value: "desktop", label: "dApps" },
-];
+]
